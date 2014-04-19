e92af396671ccb2ad376cfb42c49320bcb28a054
ravishekhargit <ravishekhar@hotmail.com>
2014-04-19 18:13:01 +0530

    Sample Code committed

---
+++ src/main/java/hadoop/book/ch4/CopyOfApp.java
@@ -0,0 +1,11 @@
+package hadoop.book.ch4;
+
+/**
+ * Hello world!
+ * 
+ */
+public class CopyOfApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/hadoop/book/ch5/CopyOfApp.java
@@ -0,0 +1,11 @@
+package hadoop.book.ch5;
+
+/**
+ * Hello world!
+ * 
+ */
+public class CopyOfApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
